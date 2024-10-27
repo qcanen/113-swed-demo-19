954c28ef680e7c161c18ac6323247004d3423ba7
qcanen <qcanen@gmail.com>
2024-10-27 21:23:51 +0800

    w07-p4: create navbar with links to class demo w02,w03,w06,and deploy it to Vercel

--- demo/w07_19.js
+++ demo/w07_19.js
@@ -1,1 +1,21 @@
-alert('hello')
+const p =document.querySelector('.show-classdemo')
+console.log('show-classdemo',p);
+
+
+function showDemo(item) {
+switch(item){
+  case 'w02_intro_19':
+    p.innerHTML =`<iframe src='./w02_intro_19/index.html
+    ' width="100%" height="100%" />`
+  break
+  case 'w03_card_19':
+    p.innerHTML =`<iframe src='./w03_card_19/
+    ' width="100%" height="100%" />`
+  break
+case 'w06_blog_19':
+  p.innerHTML =`<iframe src='./w06_blog_19/
+  ' width="100%" height="100%" />`
+  break
+}
+
+}
